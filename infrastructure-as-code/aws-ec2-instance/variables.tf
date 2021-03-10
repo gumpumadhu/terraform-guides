fed1454fd60ff5e61439d7c6249cdef9d33ee961
Avinash Varma <avinash@rafay.co>
2021-03-10 12:31:07 +0530

    az var

--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -3,9 +3,14 @@ variable "aws_region" {
   default = "us-west-1"
 }
 
+variable "aws_region_az" {
+  description = "AWS region AZ"
+  default = "us-west-1b"
+}
+
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-07585467"
 }
 
 variable "instance_type" {
@@ -15,5 +20,5 @@ variable "instance_type" {
 
 variable "name" {
   description = "name to pass to Name tag"
-  default = "Provisioned by Terraform"
+  default = "avinash-infra-provisioner"
 }
