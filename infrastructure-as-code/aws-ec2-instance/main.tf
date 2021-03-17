7fc8f578e038813d40ba38b5fd03ac97c918e8d3
Avinash Varma <avinash@rafay.co>
2021-03-17 20:55:31 +0530

    test

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -1,17 +1,23 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region_az}"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.aws_region_az
 
   tags = {
-    Name = "${var.name}"
+    Name = var.name
   }
 }
