798315c9aeeb256d82f1966d3869d4db1042e8b9
Avinash Varma <avinash@rafay.co>
2021-04-20 20:39:39 +0530

    test

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -22,3 +22,14 @@ resource "aws_instance" "ubuntu" {
     Name = var.name
   }
 }
+
+resource "aws_instance" "ubuntu-1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.aws_region_az
+
+  tags = {
+    Name = "avinash-terraform"
+  }
+  depends_on = [aws_instance.ubuntu]
+}
